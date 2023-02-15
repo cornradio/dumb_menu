215d28772687545882ee0c51a24e1f05541df06c
kasusa <kasusaland@gmail.com>
2023-02-15 16:36:34 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# dumb_menu
+one day i want to make a menu for my progarm, but i don't want to use any library they are so heavy and not support both mac and windows (i am talking about u : simple_term_menu ), so i make this.
+
+dumb_menu is a ligh weight menu ,support hot key, support both win and mac ,not tested on linux.
+
+```python
+options = ["[1]Option 1", "[2]Option 2", "[3]Option 3","[q]quit"]
+index = get_menu_choice(options)
+print(f"You selected option {index + 1}: {options[index]}")
+```
+
+![png](https://raw.githubusercontent.com/cornradio/imgs/main/20230214163952.png)
+
+## Installation
+
+https://pypi.org/project/dumb-menu/
+
---
+++ setup.py
@@ -0,0 +1,36 @@
+import codecs
+import os
+from setuptools import setup, find_packages
+
+# these things are needed for the README.md show on pypi
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+
+VERSION = '1.0'
+DESCRIPTION = 'a ligh weight menu , support both win and mac '
+LONG_DESCRIPTION = 'dumb_menu is a ligh weight menu ,support hot key, support both win and mac'
+
+# Setting up
+setup(
+    name="dumb_menu",
+    version=VERSION,
+    author="clever chen",
+    author_email="",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'menu', 'dumb_menu','windows','mac','linux'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
